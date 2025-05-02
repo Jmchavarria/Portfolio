e3716b30e7d8581635aafb4d1ec6c9ddf9e558cc
jmchavarria <marlon05chavarria@gmail.com>
2025-05-02 09:41:49 -0500

    Ahora si

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  // Otras configuraciones necesarias
-};
-
-module.exports = nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const configure = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
+  return nextConfig;
+};
+
+export default configure();
