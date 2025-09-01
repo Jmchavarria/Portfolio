bbbdc19627c4a6c6e58a6d5bd154d409bd542cda
jmchavarria <marlon05chavarria@gmail.com>
2025-09-01 09:22:52 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/myProjects.tsx
+++ src/app/myProjects.tsx
@@ -161,17 +161,22 @@ const projects: Project[] = [
     title: "CGE Currency Global",
     shortDescription: "Sistema que gestiona el intercambio de monedas.",
     longDescription: "CGE es un software dedicado a la gestión de intercambios de monedas, permitiendo a los usuarios realizar conversiones entre diferentes divisas con tarifas competitivas.",
-    link: "https://cge-exchange.greenstudio.workers.dev/",
-    imageUrl: "/images/cge/cge1.png",
+    link: "https://cge-exchange-development.greenstudio.workers.dev/",
+    imageUrl: "/images/cge/mainpage.png",
     additionalImages: [
-      "/images/cge/cge1.png",
+      "/images/cge/mainpage.png",
+      "/images/cge/login.png",
+      "/images/cge/signUp.png",
+      "/images/cge/step1cart.png",
+      "/images/cge/addressInformation.png",
+      "/images/cge/pay.png",
       "/images/cge/cge2.png",
       "/images/cge/cge3.png",
       "/images/cge/cge4.png",
       "/images/cge/cge5.png"
     ],
-    technologies: ["React", "Node.js", "ORM Prisma", "TailwindCSS", "Cloudflare Workers"],
-    features: ["Conversión de divisas", "Historial de transacciones", "Sistema de autenticación", "Panel administrativo"]
+    technologies: ["Next.js", "Node.js", "Supabase", "TailwindCSS", "Cloudflare Workers"],
+    features: ["Conversión de divisas", 'Carrito de compras', "Historial de transacciones", "Sistema de autenticación", "Panel administrativo"]
   }
 ];
 
