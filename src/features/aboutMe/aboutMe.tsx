86a843c4a341419a4447cd3e1952bae39a245f6e
jmchavarria <marlon05chavarria@gmail.com>
2026-01-23 16:24:10 -0500

    Eliminé el archivo section.tsx

--- src/features/aboutMe/aboutMe.tsx
+++ src/features/aboutMe/aboutMe.tsx
@@ -1,13 +1,9 @@
-import { Section } from '@/app/components/section';
 import { Content } from './components/content';
 
 const AboutMe = () => {
 
 
   return (
-  
-
-
     <section id="about"  className="w-full py-16 px-6 md:px-12  text-white ">
       <Content />
     </section>
